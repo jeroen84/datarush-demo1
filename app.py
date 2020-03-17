348be91b1316c2c36e21391410c8f062faa0143c
Jeroen van de Erve <jeroenvde@gmail.com>
2020-03-17 10:36:47 +0000

    changed db to sqlite from csv, removed webscraping

--- app.py
+++ app.py
@@ -4,89 +4,25 @@ import dash_bootstrap_components as dbc
 from plotly_express import line
 from plotly.subplots import make_subplots
 import pandas as pd
-
+import sqlite3
 
 _URL = {"ABP": "https://www.abp.nl/over-abp/financiele-situatie/dekkingsgraad/",
         "PFZW": "https://www.pfzw.nl/over-ons/dit-presteren-we/dekkingsgraad.html"}
 
-
-def getActueleDekkingsgraden():
-
-    # dic for changing the date (Maanden) values
-    _DIC = {"januari ": "31-01-",
-            "februari ": "28-02-",
-            "maart ": "31-03-",
-            "april ": "30-04-",
-            "mei ": "31-05-",
-            "juni ": "30-06-",
-            "juli ": "31-07-",
-            "augustus ": "31-08-",
-            "september ": "30-09-",
-            "oktober ": "31-10-",
-            "november ": "30-11-",
-            "december ": "31-12-"}
-
-    # get data from html pages
-
-    abp_df = pd.read_html(_URL["ABP"],
-                          header=0,
-                          thousands=".",
-                          decimal=",")[0]
-
-    pfzw_df = pd.read_html(_URL["PFZW"],
-                           header=0,
-                           thousands=".",
-                           decimal=",")[0]
-
-    # change column values from string to float
-    abp_df["Dekkingsgraad"] = abp_df["Dekkingsgraad"].str.replace(",", ".")
-    abp_df["Dekkingsgraad"] = abp_df["Dekkingsgraad"].str.rstrip("%").astype(float)
-
-    # manipulate certain columns in order to process properly
-    pfzw_df["Maand"] = pfzw_df["Maand"].str.lower()
-    pfzw_df["Maand"] = pfzw_df["Maand"].str.strip("\u200b")
-
-    pfzw_df["Dekkingsgraad"] = \
-        pfzw_df["Dekkingsgraad"].str.strip("\u200b")  # remove width spaces
-    pfzw_df["Dekkingsgraad"] = \
-        pfzw_df["Dekkingsgraad"].str.replace(",", ".")
-    pfzw_df["Dekkingsgraad"] = \
-        pfzw_df["Dekkingsgraad"].str.rstrip("%").astype(float)
-
-    # merge two dataframes
-    df = abp_df.merge(pfzw_df,
-                      how="left",
-                      left_on="Maanden",
-                      right_on="Maand",
-                      suffixes=("_ABP", "_PFZW"))
-
-    # keep the necessary columns
-    df = df[["Maanden",
-             "Dekkingsgraad_ABP",
-             "Dekkingsgraad_PFZW"]].dropna()
-
-    # find and replace
-    for x in _DIC:
-        df["Maanden"] = df["Maanden"].str.replace(x, _DIC[x])
-
-    # change strings to datetime format
-    df["Maanden"] = df["Maanden"].astype('datetime64[ns]')
-
-    # rename column Maanden to Datum
-    df.rename(columns={"Maanden": "Datum",
-                      "Dekkingsgraad_ABP": "ABP",
-                      "Dekkingsgraad_PFZW": "PFZW"}, inplace=True)
-
-    # need to return a melted df in order for
-    # plotly to plot multiple lines in chart
-    df_melt = df.melt(id_vars="Datum", value_vars=["ABP", "PFZW"])
-    df_melt.rename(columns={"value": "dekkingsgraad",
-                            "variable": "fonds"}, inplace=True)
-    return df_melt
-
-
 # load the dataset, ignoring empty datapoints
-df = pd.read_csv("marketdata.csv", index_col="Date").dropna()
+conn = sqlite3.connect("marketdata.db")
+_query = "SELECT date, name, value FROM marketdata"
+df = pd.read_sql(_query, conn, index_col="date")
+df = df.pivot_table(values="value", index="date", columns="name").dropna()
+
+# load the dekkingsgraden dataset
+_query2 = "SELECT date, name, value FROM dekkingsgraad"
+df_dgr = pd.read_sql(_query2, conn).sort_values("date")
+df_dgr["value"] = df_dgr["value"] * 100
+df_dgr.rename(columns={"name": "fonds",
+                       "value": "dekkingsgraad"}, inplace=True)
+# close connection
+conn.close()
 
 # create a df with the correlations
 df_corr = pd.DataFrame(df["FTSEAW"].rolling(30).corr(df["EUSA30"]))
@@ -103,14 +39,14 @@ fig.update_layout(title_text="Equities vs interest rates")
 
 # create the second graph, showing the correlation
 fig_corr = line(df_corr,
-                x="Date",
+                x="date",
                 y="Correlation",
                 title="Correlation Equities vs interest rates")
 
 # create the third graph, showing the 'Dekkingsgraden'
-df_dgr = getActueleDekkingsgraden()
+
 fig_dgr = line(df_dgr,
-               x="Datum",
+               x="date",
                y="dekkingsgraad",
                color="fonds",
                title="Actuele dekkingsgraden")
